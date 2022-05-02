ca26aecde4cf0eb030714f009711d3d4e3798322
Sana Mohiuddin <smohiuddin1688@gmail.com>
2022-05-01 20:55:54 -0700

    commit tree 23b275d7b8bae764227ff90f98251100daeffaef

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+let totalBatteries = 0;
+
+function reducer() {
+    const batteryCount = (accumulator, element) => {
+        return element + accumulator;
+    }
+    
+    return batteryBatches.reduce(batteryCount, 0);
+}
+
+totalBatteries = reducer();
